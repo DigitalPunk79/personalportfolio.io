3d1ff36190ba157f6dabd7f7c6378628430a964c
Rachel Franklin <rachel.franklin@marvelapp.com>
2024-03-07 20:36:58 +0000

    only set item to local storage once

--- colorchange.js
+++ colorchange.js
@@ -1,30 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modeBtn = document.getElementById('switch');
 
-const modeBtn = document.getElementById('switch');
+    // on first render, 
+    modeBtn.onchange = (e) => {
+        if (modeBtn.checked === true) {
+            document.documentElement.classList.remove("dark")
+            document.documentElement.classList.add("light")
+            window.localStorage.setItem('mode', 'light')
+        } else {
+            document.documentElement.classList.remove("light")
+            document.documentElement.classList.add("dark")
+            window.localStorage.setItem('mode', 'dark')
 
+        }
+    };
 
-modeBtn.onchange = (e) => {
-    if (modeBtn.checked === true) {
-        document.documentElement.classList.remove("dark")
-        document.documentElement.classList.add("light")
-        window.localStorage.setItem('mode', 'light')
-    } else {
+    const mode = window.localStorage.getItem('mode');
+    if (!mode) {
+        console.log('inside')
+        document.documentElement.classList.add("dark")
+    } else if (mode == 'dark') {
+        modeBtn.checked = false;
         document.documentElement.classList.remove("light")
         document.documentElement.classList.add("dark")
-        window.localStorage.setItem('mode', 'dark')
-
+    } else if (mode == 'light') {
+        modeBtn.checked = true;
+        document.documentElement.classList.remove("dark")
+        document.documentElement.classList.add("light")
     }
-};
-
-const mode = window.localStorage.getItem('mode');
-
-if (mode == 'dark') {
-    modeBtn.checked = false;
-    document.documentElement.classList.remove("light")
-    document.documentElement.classList.add("dark")
-}
-
-if (mode == 'light') {
-    modeBtn.checked = true;
-    document.documentElement.classList.remove("dark")
-    document.documentElement.classList.add("light")
-};
+}); 
