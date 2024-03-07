529268bbb6fa1fe4a5493d22822f32c446a539a2
Rachel Franklin <rachel.franklin@marvelapp.com>
2024-03-07 20:36:08 +0000

    wait for document to load before loading javascript

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
-var counterContainer = document.querySelector(".website-counter");
+document.addEventListener("DOMContentLoaded", function () {
+    var counterContainer = document.querySelector(".website-counter");
 
-var visitCount = localStorage.getItem("page_view");
+    var visitCount = localStorage.getItem("page_view");
 
-visitCount = 1;
-
-//Add entry for key="page_view"
-localStorage.setItem("page_view", 1);
-
-visitCount = Number(visitCount) + 1;
-
-// Update local storage value
-localStorage.setItem("page_view", visitCount);
-
-counterContainer.innerHTML = visitCount;
+    if (!visitCount) {
+        visitCount = 1
+    } else {
+        visitCount = Number(visitCount) + 1
+    };
+    //Add entry for key="page_view"
+    localStorage.setItem("page_view", visitCount);
 
 
+    // // Update local storage value
+    // localStorage.setItem("page_view", visitCount);
+
+    counterContainer.innerHTML = visitCount;
+});
 
 
 
