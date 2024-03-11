4663dd6bbe210b50b626cced84344603c96462bc
Simon Shields <digitalpunk.dev@outlook.com>
2024-03-11 11:49:06 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
-var counterContainer = document.querySelector(".website-counter");
 document.addEventListener("DOMContentLoaded", function () {
     var counterContainer = document.querySelector(".website-counter");
 
-var visitCount = localStorage.getItem("page_view");
     var visitCount = localStorage.getItem("page_view");
 
-visitCount = 1;
     if (!visitCount) {
         visitCount = 1
     } else {
@@ -14,19 +11,9 @@ visitCount = 1;
     //Add entry for key="page_view"
     localStorage.setItem("page_view", visitCount);
 
-//Add entry for key="page_view"
-localStorage.setItem("page_view", 1);
-
-visitCount = Number(visitCount) + 1;
-
-// Update local storage value
-localStorage.setItem("page_view", visitCount);
-
-counterContainer.innerHTML = visitCount;
 
     // // Update local storage value
     // localStorage.setItem("page_view", visitCount);
 
     counterContainer.innerHTML = visitCount;
 });
-
